7418c75b4eca2b4aea5958c810f3a5dc0618157d
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2026-04-20 15:56:53 +0500

    Implemented Light Rays from React Bits: https://www.reactbits.dev/backgrounds/light-rays

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 const Page = () => {
     return (
-        <div>
-          <h1>Hello World, to the Next.js 16!!</h1>
-        </div>
+        <section>
+          <h1 className="text-center">The Hub for Every Dev <br /> Even You Can't Miss</h1>
+          <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place.</p>
+        </section>
     )
 }
 export default Page
